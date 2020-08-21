8adbb5d8144db3b258b44a651917262131f747c5
Flavien PERIER <flavien.perier@pm.me>
2020-08-21 18:02:21 +0200

    Correct DNS configuration

---
+++ images/proxy/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+chown -R squid:squid /var/squid
+
+DNS_IP=`ping dns -c 1 | head -n 1 | cut -f 2 -d "(" | cut -f 1 -d ")"`
+
+cat /etc/squid/squid.conf | sed s/"^dns_nameservers .*"/"dns_nameservers $DNS_IP"/ > /tmp/squid.conf
+cat /tmp/squid.conf > /etc/squid/squid.conf
+rm /tmp/squid.conf
+
+squid -N -z
+squid -N -S -d 1
